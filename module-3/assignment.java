2541fc3bd17cc8931543250767fa2f1c0b9c902d
Angel <adavila0703@gmail.com>
2022-07-23 15:24:40 -0500

    stuff

--- module-3/assignment.java
+++ module-3/assignment.java
@@ -1,20 +1,16 @@
-import java.util.Scanner;
 
-public class RangeDetector {
+public class LogicalTest {
   public static void main(String[] args) {
-    Scanner scnr = new Scanner(System.in);
     int x;
 
-    x = scnr.nextInt();
+    x = 4;
 
-    if (x < 5) {
-      System.out.println("t");
-    } else if (x == 5) {
-      System.out.println("f");
-    } else if (x <= 25) {
-      System.out.println("i");
-    } else {
-      System.out.println("d");
+    if (!((x > 6) || (x < 3))) {
+      System.out.println("a");
     }
+    if ((x > 7) && (x < 3)) {
+      System.out.println("b");
+    }
+    System.out.println("c");
   }
 }
