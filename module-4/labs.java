83e72e15c77a87af46a745c62b3318255bddc053
Angel <adavila0703@gmail.com>
2022-07-24 21:36:33 -0500

    wip

--- module-4/labs.java
+++ module-4/labs.java
@@ -1,143 +1,61 @@
-import java.util.HashMap;
 import java.util.Scanner;
 
 public class labs {
-  /*
-   * This program outputs a downwards facing arrow composed of a rectangle and a
-   * right triangle. The arrow dimensions are defined by user specified arrow base
-   * height, arrow base width, and arrow head width.
-   * 
-   * (1) Modify the given program to use a loop to output an arrow base of height
-   * arrowBaseHeight. (1 pt)
-   * 
-   * (2) Modify the given program to use a loop to output an arrow base of width
-   * arrowBaseWidth. Use a nested loop in which the inner loop draws the *’s, and
-   * the outer loop iterates a number of times equal to the height of the arrow
-   * base. (1 pt)
-   * 
-   * (3) Modify the given program to use a loop to output an arrow head of width
-   * arrowHeadWidth. Use a nested loop in which the inner loop draws the *’s, and
-   * the outer loop iterates a number of times equal to the height of the arrow
-   * head. (2 pts)
-   * 
-   * (4) Modify the given program to only accept an arrow head width that is
-   * larger than the arrow base width. Use a loop to continue prompting the user
-   * for an arrow head width until the value is larger than the arrow base width.
-   * (1 pt)
-   * 
-   * while (arrowHeadWidth <= arrowBaseWidth) {
-   * // Prompt user for a valid arrow head value
-   * }
-   * 
-   * 
-   * Example output for arrowBaseHeight = 5, arrowBaseWidth = 2, and
-   * arrowHeadWidth = 4:
-   * 
-   * Enter arrow base height:
-   * 5
-   * Enter arrow base width:
-   * 2
-   * Enter arrow head width:
-   * 4
-   **
-   **
-   **
-   **
-   **
-   ****
-   ***
-   **
-   *
-   * 
-   * 
-   */
-
-  public static String repeatCharacter(String character, int count) {
-    return new String(new char[count]).replace("\0", character);
-  }
-
-  public static void DrawArrowBase(String character, Integer height, Integer width) {
-    for (int i = 0; i < height; i++) {
-      System.out.println(repeatCharacter(character, width));
-    }
-  }
-
-  public static void DrawArrowHead(String character, Integer width) {
-    for (int i = width; i > 0; i--) {
-      System.out.println(repeatCharacter(character, i));
-    }
-  }
-
-  public static void DrawHalfArrow() {
-    Scanner scnr = new Scanner(System.in);
-    int arrowBaseHeight;
-    int arrowBaseWidth;
-    int arrowHeadWidth;
-    String character = "*";
-
-    System.out.println("Enter arrow base height:");
-    arrowBaseHeight = scnr.nextInt();
-
-    System.out.println("Enter arrow base width:");
-    arrowBaseWidth = scnr.nextInt();
-
-    System.out.println("Enter arrow head width:");
-    arrowHeadWidth = scnr.nextInt();
-    while (arrowHeadWidth <= arrowBaseWidth) {
-      System.out.println("Enter arrow head width:");
-      arrowHeadWidth = scnr.nextInt();
-    }
-    System.out.println("");
-
-    DrawArrowBase(character, arrowBaseHeight, arrowBaseWidth);
-    DrawArrowHead(character, arrowHeadWidth);
-  }
 
   /*
-   * Given a line of text as input, output the number of characters excluding
-   * spaces, periods, exclamation points, or commas.
+   * (1) Prompt the user to enter five numbers, being five people's weights. Store
+   * the numbers in an array of doubles. Output the array's numbers on one line,
+   * each number followed by one space. (2 pts)
    * 
-   * Ex: If the input is:
+   * Ex:
    * 
-   * Listen, Mr. Jones, calm down.
-   * the output is:
+   * Enter weight 1: 236
+   * Enter weight 2: 89.5
+   * Enter weight 3: 142
+   * Enter weight 4: 166.3
+   * Enter weight 5: 93
    * 
-   * 21
-   * Note: Account for all characters that aren't spaces, periods, exclamation
-   * points, or commas (Ex: "r", "2", "?").
+   * You entered: 236.0 89.5 142.0 166.3 93.0
+   * (2) Also output the total weight, by summing the array's elements. (1 pt)
+   * 
+   * (3) Also output the average of the array's elements. (1 pt)
+   * 
+   * (4) Also output the max array element. (2 pts)
+   * 
+   * 
+   * Ex:
+   * 
+   * Enter weight 1: 236
+   * Enter weight 2: 89.5
+   * Enter weight 3: 142
+   * Enter weight 4: 166.3
+   * Enter weight 5: 93
+   * 
+   * You entered: 236.0 89.5 142.0 166.3 93.0
+   * Total weight: 726.8
+   * Average weight: 145.35999999999999
+   * Max weight: 236.0
    */
 
-  public static Integer CountCharacters(HashMap<String, String> ignoreList, String userText) {
-    Integer count = 0;
-    for (Character character : userText.toCharArray()) {
-      if (ignoreList.get(character.toString()) != null) {
-        continue;
-      }
+  public static Double[] GetWeights(Integer numberOfWeights, Scanner scnr) {
+    Double[] weights;
+    String message = "Enter weight %s: ";
 
-      count++;
+    for (int i = 0; i < numberOfWeights; i++) {
+      System.out.printf(message, i);
+      weights.
+      scnr.nextDouble();
     }
-    return count;
+
+    return weights
   }
 
-  public static void LabProgram() {
+  public static void PeopleWeights() {
     Scanner scnr = new Scanner(System.in);
-    String userText;
-    // Add more variables as needed
-
-    userText = scnr.nextLine(); // Gets entire line, including spaces.
-
-    HashMap<String, String> ignoreList = new HashMap<String, String>();
-    ignoreList.put(" ", " ");
-    ignoreList.put(".", ".");
-    ignoreList.put(",", ",");
-
-    Integer count = CountCharacters(ignoreList, userText);
-    System.out.println(count);
 
   }
 
   public static void main(String[] args) {
-    // DrawHalfArrow();
-    LabProgram();
+    PeopleWeights();
   }
 }
