9ca849ab7a66d64d3c3955f9f74da690edf8f107
Angel <adavila0703@gmail.com>
2022-08-07 20:29:31 -0500

    working on week 6 still

--- module-6/labs.java
+++ module-6/labs.java
@@ -1,4 +1,5 @@
 
+import java.sql.Date;
 import java.util.Scanner;
 
 public class labs {
@@ -31,8 +32,66 @@ public class labs {
     }
   }
 
-  public static void parsingDates() {
+  public static int getMonthAsInt(String monthString) {
+    int monthInt;
 
+    // Java switch/case statement
+    switch (monthString) {
+      case "January":
+        monthInt = 1;
+        break;
+      case "February":
+        monthInt = 2;
+        break;
+      case "March":
+        monthInt = 3;
+        break;
+      case "April":
+        monthInt = 4;
+        break;
+      case "May":
+        monthInt = 5;
+        break;
+      case "June":
+        monthInt = 6;
+        break;
+      case "July":
+        monthInt = 7;
+        break;
+      case "August":
+        monthInt = 8;
+        break;
+      case "September":
+        monthInt = 9;
+        break;
+      case "October":
+        monthInt = 10;
+        break;
+      case "November":
+        monthInt = 11;
+        break;
+      case "December":
+        monthInt = 12;
+        break;
+      default:
+        monthInt = 0;
+    }
+
+    return monthInt;
+  }
+
+  public static void dateFormatCheck(String dateString) {
+    // dateString.substring(beginIndex)
+  }
+
+  public static void parsingDates() {
+    Scanner scnr = new Scanner(System.in);
+
+    // TODO: Read dates from input, parse the dates to find the ones
+    // in the correct format, and output in m/d/yyyy format
+    String test = "March 1, 1990";
+    String woop = test.substring(1);
+    System.out.println(woop);
   }
 
   public static void main(String[] args) {
